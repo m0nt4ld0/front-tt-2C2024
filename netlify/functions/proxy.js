7c29d1ee58a565b8c178b33450bb97497cc333ed
m0nt4ld0 <mmontaldo@live.com>
2024-12-13 14:20:29 -0300

    Fix: Configuracion de API para netflify

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,8 +1,21 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
-    const response = await fetch('http://api.geonames.org/neighboursJSON?formatted=true&geonameId=2658434&username=mmontaldo' + event.path);
+    const fetch = (await import('node-fetch')).default;
+    console.log('Event:', event);
+    const baseUrl = 'http://api.geonames.org/neighboursJSON';
+    const queryParams = '?formatted=true&geonameId=2658434&username=mmontaldo';
+    const response = await fetch(baseUrl + queryParams);
+    console.log('Response status:', response.status);
     const data = await response.json();
+    console.log('Response data:', data);
+    console.log('Data fetched from GeoNames:', data);
+
+    if (!data || !data.geonames) {
+        console.error('Invalid data structure:', data);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Invalid data structure from GeoNames' }),
+        };
+    }
 
     return {
         statusCode: 200,
